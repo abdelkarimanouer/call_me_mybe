fd3cbe631e9f909a0a5dac4bee96ccc478efb53b
abdelkarimanouer <abdelkarimanouer1337@gmail.com>
2026-07-17 11:59:14 +0100

    update file

--- src/__main__.py
+++ src/__main__.py
@@ -9,11 +9,9 @@ def main() -> None:
     parse_fun_def = Parsing.load_json_data(arguments['fun_def'])
     parse_input_tests = Parsing.load_json_data(arguments['input'])
 
-    # model: Small_LLM_Model = Small_LLM_Model()
+    model: Small_LLM_Model = Small_LLM_Model()
 
     prompt = get_full_prompt("What is the sum of 2 and 3?", parse_fun_def)
-
-    print(prompt)
 
 
 if __name__ == "__main__":
