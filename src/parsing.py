b460d0594530f50bc4808fb341ad59f085185189
abdelkarimanouer <abdelkarimanouer1337@gmail.com>
2026-07-22 09:36:11 +0100

    handle some edge cases on parsing

--- src/parsing.py
+++ src/parsing.py
@@ -1,6 +1,8 @@
-from typing import List, Dict
+from typing import List, Dict, Any
 import json
 import argparse
+import sys
+import regex
 
 
 class Parsing:
@@ -25,13 +27,31 @@ class Parsing:
         return arguments
 
     @staticmethod
+    def check_input_tests(t: Any) -> bool:
+        if not isinstance(t, dict) or len(t) != 1:
+            return False
+
+        for key in t:
+            if bool(regex.match("^prompt$", str(key))) and t[key]:
+                return True
+            else:
+                return False
+
+    @staticmethod
     def get_input_tests(path_file: str) -> List:
         try:
             with open(path_file, "r") as f:
                 tests = json.load(f)
                 input_tests = []
                 for t in tests:
-                    input_tests.append(t["prompt"])
+                    if Parsing.check_input_tests(t) is False:
+                        print("""[ERROR]: Invalid test input. \
+Please enter it like this {\"Prompt\": \"<your prompt>\"} \
+""", file=sys.stderr)
+                        exit()
+                    else:
+                        for k in t:
+                            input_tests.append(t[k])
                 return input_tests
         except json.JSONDecodeError as e:
             print(f"[ERROR]: {e}")
@@ -44,10 +64,16 @@ class Parsing:
             exit()
 
     @staticmethod
+    def check_funs_def(funs_def: Any) -> bool:
+        ...
+
+    @staticmethod
     def get_funs_definition(path_file: str) -> List:
         try:
             with open(path_file, "r") as f:
-                return json.load(f)
+                funs_def = json.load(f)
+                if Parsing.check_funs_def(funs_def):
+                    return funs_def
         except json.JSONDecodeError as e:
             print(f"[ERROR]: {e}")
             exit()
