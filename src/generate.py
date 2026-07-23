a1815f9e9be4dd7a28a1c8568409b5d9b7ac2fbc
abdelkarimanouer <abdelkarimanouer1337@gmail.com>
2026-07-23 02:18:26 +0100

    start generate the model

--- src/generate.py
+++ src/generate.py
@@ -44,25 +44,4 @@ class Generate:
     @staticmethod
     def run_generate(input_tests: List, funs_def: List) -> None:
         model: Small_LLM_Model = Small_LLM_Model()
-
-        steps = {
-            1: model.encode('{\n\t"prompt": ')[0].tolist(),
-            2: [model.encode(f'"{p}",')[0].tolist() for p in input_tests],
-            3: model.encode('\n\t"name": ')[0].tolist(),
-        }
-
-        l_prompts = len(input_tests)
-        for i in range(l_prompts):
-            full_ids = []
-            for s in range(1, len(steps) + 1):
-                if s == 1:
-                    prompt_ids = steps[s]
-                    full_ids.extend(prompt_ids)
-                elif s == 2:
-                    ids = steps[s][i]
-                    full_ids.extend(ids)
-                elif s == 3:
-                    name_ids = steps[s]
-                    full_ids.extend(name_ids)
-
-            print(model.decode(full_ids))
+        ...
